95e5ed21a62f99bd7ba3dfee43cef96d589659e1
Loïc Antignac <contact@webaxones.com>
2022-08-05 09:10:40 +0200

    add test to check if constant WEBAXONES_VENDOR_PATH is already declared

--- src/Library.php
+++ src/Library.php
@@ -22,8 +22,10 @@ class Library
 	 */
 	public static function init( string $textDomain ): void
 	{
-		$vendorPath = substr( __FILE__, 0, strpos( __FILE__, 'vendor\\' ) ) . 'vendor\\';
-		define( 'WEBAXONES_VENDOR_PATH', $vendorPath );
+		if ( ! defined( 'WEBAXONES_VENDOR_PATH' ) ) {
+			$vendorPath = substr( __FILE__, 0, strpos( __FILE__, 'vendor\\' ) ) . 'vendor\\';
+			define( 'WEBAXONES_VENDOR_PATH', $vendorPath );
+		}
 
 		Decalog::initLibrary( 'webaxones-entities', 'Webaxones Entities Library', '1.0.0' );
 
